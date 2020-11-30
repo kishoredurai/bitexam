9a1e9e63a8289dda779dbd11bc3031eabc9dec1f
kishoredurai <kishoredurai7@gmail.com>
2020-11-30 23:10:09 +0530

    index page designed

--- index.php
+++ index.php
@@ -1,9 +1,46 @@
+<?php
 
 
-			<p><a href="student/register.php" class="btn btn-warning btn-lg">Register</a></p>
-			<p><a href="student/login.php" class="btn btn-dark btn-lg">Login</a></p>
+include('student/header.php');
+
+?>
+
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+
+<link href="style/button.css" rel="stylesheet" type="text/css">
+<hr>
+<div class="container">
+
+	<div class="row">
+		<div class="col-md-3">
+
+		</div>
+		<div class="col-md-6" style="margin-top:50px;">
 
 
-</body>
+		<h1 style="align-content: center;font-size:50px;font-family:Comic Sans MS;" align="center">Student Login</h1><br>
 
-</html>
+			<div class="form-group" align="center">
+
+				<a href="student/login.php" class="btn success">Login</a>&nbsp;
+				<a class="btn blue" href="student/register.php">Register</a>
+			</div>
+			<br><br>
+			<h1 style="align-content: center;font-size:50px;font-family:Comic Sans MS;" align="center">Faculty Login</h1><br>
+
+<div class="form-group" align="center">
+
+	<a href="master/login.php" class="btn success">Login</a>&nbsp;
+	<a class="btn blue" href="master/register.php">Register</a>
+</div>
+
+			<div class="col-md-3">
+
+			</div>
+		</div>
+	</div>
+
+	</body>
+
+	</html>
